b998999ec809e35ea40d3a27c60d31ba369f8b52
Jazmin420 <jashirajasmim@gmail.com>
2024-04-04 22:40:06 +0530

    crud

--- json-server/index.js
+++ json-server/index.js
@@ -4,6 +4,10 @@ const middleware = jsonServer.defaults()
 const router = jsonServer.router('db.json')
 const PORT = 3000 || process.env.PORT
 
+var pathToModule = require.resolve('json-server');
+console.log(pathToModule);
+
+
 CEServer.use(middleware)
 CEServer.use(router)
 
